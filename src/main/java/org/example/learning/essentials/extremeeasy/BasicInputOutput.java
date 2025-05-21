bc169b3146ebf825f927c67374fa084f0a6e577a
Andrzej Lewiński <andrzej_lewinski@o2.pl>
2025-05-21 15:45:30 +0200

    git commit -m "methods and functions - extreme easy"

---
+++ src/main/java/org/example/learning/essentials/extremeeasy/BasicInputOutput.java
@@ -0,0 +1,24 @@
+package org.example.learning.essentials.extremeeasy;
+
+import org.example.learning.QuickStart;
+import org.example.learning.utils.animations.launcher.AnimationLauncher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by User on 21.05.2025
+ */
+public class BasicInputOutput {
+
+    private static final Logger logger = LoggerFactory.getLogger(BasicInputOutput.class);
+
+
+    public static void main(String[] args) {
+        logger.info("🚀 Program starts...");
+        ///  AnimationLauncher.runDots();
+        AnimationLauncher.runHerbalLeaf();
+        ///  AnimationLauncher.runClover();
+        ///AnimationLauncher.runLemon();
+    }
+
+}
