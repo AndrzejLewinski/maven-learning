d3703d1bd109df64d59b416e228c9907086db7ae
Andrzej Lewiński <andrzej_lewinski@o2.pl>
2025-05-23 01:00:48 +0200

    git commit -m "."

--- src/main/java/org/example/learning/essentials/extremeeasy/IntroductionToJava/LearningOnly/ChristmasTree.java
+++ src/main/java/org/example/learning/essentials/extremeeasy/IntroductionToJava/LearningOnly/ChristmasTree.java
@@ -19,25 +19,13 @@ public class ChristmasTree {
         ///AnimationLauncher.runLemon();
 
         ChristmasTree christmasTree = new ChristmasTree();
-        christmasTree.christmasTree();
-
+        christmasTree.exercises();
     }
 
+    private void exercises() {
 
-    private void christmasTree() {
-        int h = 4;
-        for (int i = 1; i <= h; i++) {
-            // Spacje
-            for (int j = 1; j <= h - i; j++) {
-                System.out.print("-");
-            }
-            // Gwiazdki
-            for (int j = 1; j <= 2 * i - 1; j++) {
-                System.out.print("*");
-            }
-            // Przejście do nowej linii
-            System.out.println();
-        }
+
+
     }
 
 
