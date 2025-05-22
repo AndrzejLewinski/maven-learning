6f4ea115e430cbb7999a10e5ea777dbf3a0e4503
Andrzej Lewiński <andrzej_lewinski@o2.pl>
2025-05-22 20:06:45 +0200

    git commit -m "Add examples of comparison operators in Java for learning purposes"

---
+++ src/main/java/org/example/learning/essentials/extremeeasy/IntroductionToJava/ComparisonOperators.java
@@ -0,0 +1,29 @@
+package org.example.learning.essentials.extremeeasy.IntroductionToJava;
+
+import org.example.learning.utils.animations.launcher.AnimationLauncher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by User on 22.05.2025
+ */
+public class ComparisonOperators {
+    private static final Logger logger = LoggerFactory.getLogger(ComparisonOperators.class);
+
+
+    public static void main(String[] args) {
+        logger.info("🚀 Program starts...");
+        ///AnimationLauncher.runDots();
+        AnimationLauncher.runHerbalLeaf();
+        ///AnimationLauncher.runClover();
+        ///AnimationLauncher.runLemon();
+        TernaryOperator ternaryOperator  = new TernaryOperator();
+
+
+    }
+
+
+
+
+
+}
