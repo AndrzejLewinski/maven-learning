1f28124dcea0921c70004594d3a41701bcc9a0c5
Andrzej Lewiński <andrzej_lewinski@o2.pl>
2025-05-22 16:59:26 +0200

    git commit -m "Add helper methods for validating user input: isValidExcludesZero and isValid "

--- src/main/java/org/example/learning/essentials/extremeeasy/IntroductionToJava/IfElseStatements.java
+++ src/main/java/org/example/learning/essentials/extremeeasy/IntroductionToJava/IfElseStatements.java
@@ -4,6 +4,8 @@ import org.example.learning.utils.animations.launcher.AnimationLauncher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Scanner;
+
 /**
  * Created by User on 22.05.2025
  */
@@ -18,8 +20,47 @@ public class IfElseStatements {
         AnimationLauncher.runHerbalLeaf();
         ///  AnimationLauncher.runClover();
         ///AnimationLauncher.runLemon();
+        IfElseStatements elseStatements = new IfElseStatements();
+        elseStatements.isGreaterThanTen();
+    }
 
+    private void isGreaterThanTen() {
+        Scanner scanner = new Scanner(System.in);
+        int x = isValidExcludesZero(scanner);
+    }
 
+    private int isValidExcludesZero(Scanner scanner){
+        int number = Integer.MIN_VALUE;
+        boolean isValid = true;
+        while (isValid){
+            try{
+                String inputString = scanner.nextLine();
+                number = Integer.parseInt(inputString);
+                if (number == 0) {
+                    System.out.println("You can't divide by zero. Enter valid number: ");
+                } else {
+                    isValid = false;
+                }
+            }catch (NumberFormatException e){
+                System.out.println("NumberFormatException : This is not a number! Enter the number: ");
+            }
+        }
+        return number;
+    }
+
+    private int isValid(Scanner scanner){
+        int number = Integer.MIN_VALUE;
+        boolean isValid = true;
+        while (isValid){
+            try{
+                String inputString = scanner.nextLine();
+                number = Integer.parseInt(inputString);
+                isValid=false;
+            }catch (NumberFormatException e){
+                System.out.println("NumberFormatException : This is not a number! Enter the number: ");
+            }
+        }
+        return number;
     }
 
 
