9f476f70feff3d78b82121cd577753b35ff93c3e
Andrzej Lewiński <andrzej_lewinski@o2.pl>
2025-05-23 14:04:02 +0200

    git commit -m "."

--- src/main/java/org/example/learning/essentials/extremeeasy/IntroductionToJava/LearningOnly/ChristmasTree.java
+++ src/main/java/org/example/learning/essentials/extremeeasy/IntroductionToJava/LearningOnly/ChristmasTree.java
@@ -1,5 +1,6 @@
 package org.example.learning.essentials.extremeeasy.IntroductionToJava.LearningOnly;
 
+import org.example.learning.essentials.extremeeasy.IntroductionToJava.LearningOnly.archiv.ChristmasTree_Archive01;
 import org.example.learning.utils.animations.launcher.AnimationLauncher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,10 +8,9 @@ import org.slf4j.LoggerFactory;
 /**
  * Created by User on 23.05.2025
  */
-@SuppressWarnings("unused")
 public class ChristmasTree {
 
-    private static final Logger logger = LoggerFactory.getLogger(ChristmasTree.class);
+    private static final Logger logger = LoggerFactory.getLogger(ChristmasTree_Archive01.class);
 
     public static void main(String[] args) {
         logger.info("🚀 Program starts...");
@@ -21,288 +21,49 @@ public class ChristmasTree {
         ///AnimationLauncher.runLemon();
 
         ChristmasTree christmasTree = new ChristmasTree();
-        christmasTree.threeChristmasTreesBase();
+        christmasTree.exercise3();
 
     }
 
+    private void exercise3() {
 
-    private void threeChristmasTreesBase() {
-        int n  = 5;
-        char stair = '*';
-        int  breaks = 1;
-        int  multiply = 1;
-        for(int i = n - 1; i >= 0; i--){
+        int loopLength = 5;
+        char sign = '*';
 
-            int x = i+1;
+        for(int i =0;i<loopLength;i++){ //  0,1,2,3,4 (loop runs 5 times)
+            System.out.print("("+i+") "); // prints the current row index (useful for debugging)
 
-            String input = String.valueOf(" ");
+            for(int j =0;j<=i;j++){ // for j<=i (j,i = 0),  prints (i + 1) "*" characters in each line
+                System.out.print(sign);
+            }
 
-            //first block of code
-            for(int j=0;j<=i;j++){
-                for(int m=0; m<multiply; m++){
-                    System.out.print(input);
-                }
-            }
-            //second block of code
-            for(int o=0; o<multiply; o++){
-                for(int m=0; m<n*2-1-i*2; m++){
-                    System.out.print(stair);
-                }
-            }
-            //third block of code
-            for(int j=0;j<=i*2;j++){
-                for(int m=0; m<multiply; m++){
-                    System.out.print(input);
-                }
-            }
-            //fourth block of code
-            for(int o=0; o<multiply; o++){
-                for(int m=0; m<n*2-1-i*2; m++){
-                    System.out.print(stair);
-                }
-            }
-            //5th block of code
-            for(int j=0;j<=i*2;j++){
-                for(int m=0; m<multiply; m++){
-                    System.out.print(input);
-                }
-            }
-            //fourth block of code
-            for(int o=0; o<multiply; o++){
-                for(int m=0; m<n*2-1-i*2; m++){
-                    System.out.print(stair);
-                }
-            }
-            for(int m =0;m<breaks;m++){
-                System.out.println();
-            }
-        }
-
-
-        for(int i = 0; i < n; i++){
-
-            int x = i+1;
-
-            String input = String.valueOf(" ");
-
-            //first block of code
-            for(int j=0;j<=i;j++){
-                for(int m=0; m<multiply; m++){
-                    System.out.print(input);
-                }
-            }
-            //second block of code
-            for(int o=0; o<multiply; o++){
-                for(int m=0; m<n*2-1-i*2; m++){
-                    System.out.print(stair);
-                }
-            }
-            //third block of code
-            for(int j=0;j<=i*2;j++){
-                for(int m=0; m<multiply; m++){
-                    System.out.print(input);
-                }
-            }
-            //fourth block of code
-            for(int o=0; o<multiply; o++){
-                for(int m=0; m<n*2-1-i*2; m++){
-                    System.out.print(stair);
-                }
-            }
-            //5th block of code
-            for(int j=0;j<=i*2;j++){
-                for(int m=0; m<multiply; m++){
-                    System.out.print(input);
-                }
-            }
-            //fourth block of code
-            for(int o=0; o<multiply; o++){
-                for(int m=0; m<n*2-1-i*2; m++){
-                    System.out.print(stair);
-                }
-            }
-            for(int m =0;m<breaks;m++){
-                System.out.println();
-            }
+            System.out.println(); // moves to the next line after printing stars
         }
     }
 
+    private void exercise2() {
 
-    private void exerciseEleven() {
+        char sign = '*';
 
-        int n  = 5;
-        char stair = '*';
-        int  breaks = 1;
-        int  multiply = 1;
-        for(int i = n - 1; i >= 0; i--){
-            //first block of code
-            for(int j=0;j<=i;j++){
-                for(int m=0; m<multiply; m++){
-                    System.out.print(i+1);
-                }
-            }
-            //second block of code
-            for(int o=0; o<multiply; o++){
-                for(int m=0; m<n*2-1-i*2; m++){
-                    System.out.print(stair);
-                }
-            }
-            //third block of code
-            for(int j=0;j<=i;j++){
-                for(int m=0; m<multiply; m++){
-                    System.out.print(i+1);
-                }
-            }
-
-            for(int m =0;m<breaks;m++){
-                System.out.println();
-            }
-        }
-
-    }
-
-    private void exerciseTen() {
-
-        int n  = 3;
-        char stair = '*';
-        int  breaks = 1;
-        int  multiply = 1;
-        for(int i = n - 1; i >= 0; i--){
-            //first block of code
-            for(int j=0;j<=i;j++){
-                for(int m=0; m<multiply; m++){
-                    System.out.print(i+1);
-                }
-            }
-            //second block of code
-            for(int o=0; o<multiply; o++){
-                for(int m=0; m<n*2-1-i*2; m++){
-                    System.out.print(stair);
-                }
-            }
-
-            for(int m =0;m<breaks;m++){
-                System.out.println();
-            }
-        }
-    }
-
-
-    private void exerciseNine() {
-        int n  = 5;
-        char stair = '@';
-        int  breaks = 1;
-        int  multiply = 1;
-        for(int i = 0; i < n; i++){
-            for(int j=0;j<=i;j++){
-                for(int m=0; m<multiply; m++){
-                    System.out.print(i+1);
-                }
-            }
-            for(int m =0;m<breaks;m++){
-                System.out.println();
-            }
-        }
-    }
-
-    private void exerciseEight() {
-        int n  = 4;
-        char stair = '@';
-        int  breaks = 1;
-        int  multiply = 1;
-        for(int i = 0; i < n; i++){
-            for(int j=0;j<=i;j++){
-                for(int m=0; m<multiply; m++){
-                    System.out.print(stair);
-                }
-            }
-            for(int m =0;m<breaks;m++){
-                System.out.println();
-            }
-        }
-    }
-
-    private void exerciseSeven() {
-        int n  = 5;
-        char stair = '*';
-        int  breaks = 2;
-        int  multiply = 1;
-        for(int i = n - 1; i >= 0; i--){
-            for(int j=0;j<=i;j++){
-                for(int m=0; m<multiply; m++){
-                   System.out.print(stair);
-               }
-            }
-            for(int m =0;m<breaks;m++){
-                System.out.println();
-            }
-        }
-    }
-    private void exerciseSix() {
-        int n  = 5;
-        char stair = '*';
-        int  breaks = 1;
-        int  multiply = 2;
-        for(int i =0;i<n;i++){
-            for(int j=0;j<=i;j++){
-                for(int m =0;m<breaks*multiply;m++){
-                    System.out.print(stair);
-                }
-            }
-            for(int m =0;m<breaks;m++){
-                System.out.println();
-            }
-        }
-    }
-
-    private void exerciseFive() {
-        int n  = 5;
-        char stair = '*';
-        int  breaks = 2;
-
-        for(int i =0;i<n;i++){
-            for(int j=0;j<=i;j++){
-                System.out.print(stair);
-            }
-            for(int m =0;m<breaks;m++){
-                System.out.println();
-            }
-        }
-    }
-    private void exerciseFour() {
-        int n  = 3;
-        char stair = '#';
-
-        for(int i =0;i<n;i++){
-            for(int j=0;j<=i;j++){
-                System.out.print(stair);
+        for(int i =0;i<3;i++){
+            for(int j =0;j<3;j++){
+                System.out.print(sign);
             }
             System.out.println();
         }
-    }
-    private void exerciseThree() {
-        int n  = 5;
-        for(int i = n - 1; i >= 0; i--){
-            for(int j=0;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-    private void exerciseTwo() {
-        int n  = 5;
-        for(int i =0;i<n;i++){
-            for(int j=0;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-    private void exerciseOne() {
-        for(int i =0;i<5;i++){
-            System.out.println("*");
-        }
+
     }
 
+    private void exercise1() {
+        char sign = '*';
+        for(int i =0;i<5;i++)
+            System.out.print(sign);
 
+    }
+
+    private void exercise0() {
+        char sign = '*';
+        for(int i =0;i<5;i++)
+            System.out.println(sign);
+    }
 }
