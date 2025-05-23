c98fe9fd574177b77d955c004942cc47cf38a1cf
Andrzej Lewiński <andrzej_lewinski@o2.pl>
2025-05-23 18:47:10 +0200

    git commit -m "wip: start learning logical operators in Java - extreme easy exercises"

--- src/main/java/org/example/learning/essentials/extremeeasy/IntroductionToJava/LogicalOperatorsInJava.java
+++ src/main/java/org/example/learning/essentials/extremeeasy/IntroductionToJava/LogicalOperatorsInJava.java
@@ -17,102 +17,13 @@ public class LogicalOperatorsInJava {
 
     public static void main(String[] args) {
         logger.info("🚀 Program starts...");
-        ///  AnimationLauncher.runDots();
         AnimationLauncher.runHerbalLeaf();
-        ///  AnimationLauncher.runClover();
-        ///AnimationLauncher.runLemon();
-
-        new LogicalOperatorsInJava().exercise();
-
     }
 
-    @SuppressWarnings("CommentedOutCode")
     private void exercise() {
 
         Scanner scanner = new Scanner(System.in);
-        // TODO: Uncomment these method calls when input validation exercises are enabled
-        // int number = readInteger(scanner);
-        // int number2 = readPositiveInt(scanner);
-        // int number3 = readDivisor(scanner);
-
-
-
-
 
 
     }
-
-    private boolean readBoolean(Scanner scanner) {
-        System.out.println("Enter boolean value (true/false): ");
-        while (true) {
-            String input = scanner.nextLine().trim().toLowerCase();
-            if (input.equals("true")) {
-                return true;
-            } else if (input.equals("false")) {
-                return false;
-            } else {
-                System.out.println("This is not a boolean value. Please enter 'true' or 'false': ");
-            }
-        }
-    }
-
-
-    private int readDivisor(Scanner scanner){
-        System.out.println("Enter integer number different than zero: ");
-        int result;
-        while (true){
-            try{
-                String string = scanner.nextLine();
-                result = Integer.parseInt(string);
-                if (result == 0) {
-                    System.out.println("You can't divide by zero. Please enter number different than zero: ");
-                } else {
-                    return result;
-                }
-            }catch (NumberFormatException e){
-                System.out.println("This is not a number. Enter valid number: ");
-            }
-        }
-    }
-
-    private int readPositiveInt(Scanner scanner){
-        System.out.println("Enter positive integer number: ");
-        int result;
-        while (true){
-            try{
-                String string = scanner.nextLine();
-                result = Integer.parseInt(string);
-                if(result>0){
-                    return result;
-                }else {
-                    System.out.println("Number must be positive. Please enter number greater than zero: ");
-                }
-            }catch (NumberFormatException e){
-                System.out.println("This is not a number. Enter valid number: ");
-            }
-        }
-    }
-
-    private int readInteger(Scanner scanner){
-        System.out.println("Enter integer number: ");
-        int result;
-        while (true){
-            try{
-             String string = scanner.nextLine();
-             result = Integer.parseInt(string);
-             return result;
-            }catch (NumberFormatException e){
-                System.out.println("This is not a number. Enter valid number: ");
-            }
-        }
-    }
-
-
-
-
-
-
-
-
-
 }
