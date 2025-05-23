d37413a8aa243abcaac716930b15aa354695bdc0
Andrzej Lewiński <andrzej_lewinski@o2.pl>
2025-05-23 19:12:46 +0200

    git commit -m "feat(logical-operators): add basic interactive exercises with validations"

--- src/main/java/org/example/learning/essentials/extremeeasy/IntroductionToJava/LogicalOperatorsInJava.java
+++ src/main/java/org/example/learning/essentials/extremeeasy/IntroductionToJava/LogicalOperatorsInJava.java
@@ -18,12 +18,84 @@ public class LogicalOperatorsInJava {
     public static void main(String[] args) {
         logger.info("🚀 Program starts...");
         AnimationLauncher.runHerbalLeaf();
+        LogicalOperatorsInJava logicalOperatorsInJava = new LogicalOperatorsInJava();
+        logicalOperatorsInJava.exercise3();
+    }
+
+    private void exercise3() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many steps have you made today ? ");
+        int stepsToday = ValidationUtils.readInteger(scanner);
+        int goal = 10000;
+        System.out.println("Do we have a bonus day ? ");
+        boolean bonusDay = ValidationUtils.readBoolean(scanner);
+        if(bonusDay || stepsToday>goal){
+            System.out.println("User will receive a gift.");
+        }
+        scanner.close();
+    }
+
+    private void exercise2() {
+        Scanner scanner = new Scanner(System.in);
+        int x = ValidationUtils.readInteger(scanner);
+        int y = ValidationUtils.readInteger(scanner);
+
+        if(x<y){
+            System.out.println(" x < y");
+        }else if(x==y){
+            System.out.println("x = y");
+        }else{
+            System.out.println("x>y");
+        }
+
+    }
+
+    private void exercise1() {
+        Scanner scanner = new Scanner(System.in);
+        boolean isRaining = false;
+        if(!isRaining){
+            System.out.println("It's raining!");
+        }else {
+            System.out.println("It's not raining!");
+        }
     }
 
     private void exercise() {
-
         Scanner scanner = new Scanner(System.in);
-
-
+        int number = ValidationUtils.readInteger(scanner);
+        isGreaterThanThree(number);
     }
+
+    private void isGreaterThanThree(int number){
+        if (number>3){
+            System.out.println(number+" is greater than three.");
+        }else {
+            System.out.println(number+" is not greater than three.");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
