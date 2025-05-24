a03bf8db3ce4243587d0afb75f9a9a6504a30c54
Andrzej Lewiński <andrzej_lewinski@o2.pl>
2025-05-24 09:30:54 +0200

    git commit -m "Add: ForEachLoopsInJava example class"

---
+++ src/main/java/org/example/learning/essentials/extremeeasy/IntroductionToJava/ForEachLoopsInJava.java
@@ -0,0 +1,24 @@
+package org.example.learning.essentials.extremeeasy.IntroductionToJava;
+
+import org.example.learning.utils.animations.launcher.AnimationLauncher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by User on 24.05.2025
+ */
+public class ForEachLoopsInJava {
+
+
+    private static final Logger logger = LoggerFactory.getLogger(ForEachLoopsInJava.class);
+
+    public static void main(String[] args) {
+        logger.info("🚀 Program starts...");
+        ///  AnimationLauncher.runDots();
+        AnimationLauncher.runHerbalLeaf();
+        ///  AnimationLauncher.runClover();
+        ///AnimationLauncher.runLemon();
+
+
+    }
+}
